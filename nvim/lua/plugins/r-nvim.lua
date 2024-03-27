3899f4ee2d0fe754a658b910dfe63c10a0698d08
joscani <canadasreche@gmail.com>
2024-03-27 18:38:58 +0100

    cambio plugin de nvimr a r-nvim

---
+++ nvim/lua/plugins/r-nvim.lua
@@ -0,0 +1,14 @@
+return {
+    {
+    "R-nvim/R.nvim",
+    lazy = false
+  },
+  "R-nvim/cmp-r",
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require("cmp").setup({ sources = {{ name = "cmp_r" }}})
+      require("cmp_r").setup({ })
+    end,
+  },
+}
